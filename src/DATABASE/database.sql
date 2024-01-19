78ea68eaad40f33991799bf2062e1c6f792072e7
Jose Manuel Aranda <jomagtr@gmail.com>
2024-01-19 01:30:21 +0100

    archivo database.sql actualizado

--- src/DATABASE/database.sql
+++ src/DATABASE/database.sql
@@ -1,3 +1,5 @@
+SET foreign_key_checks = 0;
+
 DROP TABLE IF EXISTS TRABAJADOR;
 DROP TABLE IF EXISTS PEDIDO;
 DROP TABLE IF EXISTS RESERVAS;
@@ -7,192 +9,113 @@ DROP TABLE IF EXISTS INGREDIENTES;
 DROP TABLE IF EXISTS RECETAS;
 DROP TABLE IF EXISTS RESERVAS_PEDIDO;
 DROP TABLE IF EXISTS TRABAJADOR_PEDIDO;
-DROP TABLE IF EXISTS CLIENTE_PEDIDO;
+DROP TABLE IF EXISTS CLIENTES_PEDIDO;
 DROP TABLE IF EXISTS PEDIDO_RECETAS;
-DROP TABLE IF EXISTS CLIENTE_ALERGENOS;
+DROP TABLE IF EXISTS CLIENTES_ALERGENOS;
 DROP TABLE IF EXISTS RECETAS_INGREDIENTES;
 DROP TABLE IF EXISTS INGREDIENTES_ALERGENOS;
+DROP TRIGGER IF EXISTS ActualizarStock;
+DROP TRIGGER IF EXISTS RestarPuntos;
+DROP TRIGGER IF EXISTS RellenarStock;
+DROP TRIGGER IF EXISTS ContieneAlergeno;
 
+SET foreign_key_checks = 1;
 
-CREATE TABLE TRABAJADOR(
-    IdTrabajador INT,
-    Turno INT,
-    PRIMARY KEY(IdTrabajador)
-);
+CREATE TABLE IF NOT EXISTS TRABAJADOR(IdTrabajador INT,Turno INT,PRIMARY KEY(IdTrabajador));
+CREATE TABLE IF NOT EXISTS PEDIDO(IdPedido INT,Valoracion INT,TPago VARCHAR(10) CHECK (TPago IN ('Tarjeta','Efectivo','Puntos')),Estado VARCHAR(10) CHECK (Estado IN ('Activo','Inactivo')),PRIMARY KEY(IdPedido));
+CREATE TABLE IF NOT EXISTS RESERVAS(IdReserva INT,IdMesa INT,PRIMARY KEY(IdReserva));
+CREATE TABLE IF NOT EXISTS CLIENTES(IdCliente INT,Valoracion INT,Nombre VARCHAR(40),UserName VARCHAR(10),Contrasenia VARCHAR(40),Domicilio VARCHAR(40),Puntos INT,FechaNacimiento DATETIME,DatosDePago VARCHAR(30),PRIMARY KEY(IdCliente));
+CREATE TABLE IF NOT EXISTS ALERGENOS(IdAlergeno INT,Nombre VARCHAR(40),Descripcion VARCHAR(40),PRIMARY KEY(IdAlergeno));
+CREATE TABLE IF NOT EXISTS INGREDIENTES(IdIngrediente INT,Nombre VARCHAR(40),NumStock INT CHECK (NumStock >= 0),PRIMARY KEY(IdIngrediente));
+CREATE TABLE IF NOT EXISTS RECETAS(IdReceta INT,Precio INT CHECK (Precio >= 1),PRIMARY KEY(IdReceta));
+CREATE TABLE IF NOT EXISTS RESERVAS_PEDIDO(IdReserva INT,IdPedido INT,NumPersonas INT,HoraIni DATETIME,PRIMARY KEY(IdPedido,IdReserva),FOREIGN KEY(IdReserva) REFERENCES RESERVAS(IdReserva),FOREIGN KEY(IdPedido) REFERENCES PEDIDO(IdPedido));
+CREATE TABLE IF NOT EXISTS TRABAJADOR_PEDIDO(IdTrabajador INT,IdPedido INT,PRIMARY KEY(IdTrabajador,IdPedido),FOREIGN KEY(IdPedido) REFERENCES PEDIDO(IdPedido),FOREIGN KEY(IdTrabajador) REFERENCES TRABAJADOR(IdTrabajador));
+CREATE TABLE IF NOT EXISTS CLIENTES_PEDIDO(IdCliente INT,IdPedido INT,PRIMARY KEY(IdPedido,IdCliente),FOREIGN KEY(IdPedido) REFERENCES PEDIDO(IdPedido),FOREIGN KEY(IdCliente) REFERENCES CLIENTES(IdCliente));
+CREATE TABLE IF NOT EXISTS PEDIDO_RECETAS(IdReceta INT,IdPedido INT,PRIMARY KEY(IdPedido,IdReceta),FOREIGN KEY(IdPedido) REFERENCES PEDIDO(IdPedido),FOREIGN KEY(IdReceta) REFERENCES RECETAS(IdReceta));
+CREATE TABLE IF NOT EXISTS CLIENTES_ALERGENOS(IdCliente INT,IdAlergeno INT,PRIMARY KEY(IdCliente,IdAlergeno),FOREIGN KEY(IdCliente) REFERENCES CLIENTES(IdCliente),FOREIGN KEY(IdAlergeno) REFERENCES ALERGENOS(IdAlergeno));
+CREATE TABLE IF NOT EXISTS RECETAS_INGREDIENTES(IdReceta INT,IdIngrediente INT,PRIMARY KEY(IdReceta,IdIngrediente),FOREIGN KEY(IdReceta) REFERENCES RECETAS(IdReceta),FOREIGN KEY(IdIngrediente) REFERENCES INGREDIENTES(IdIngrediente));
+CREATE TABLE IF NOT EXISTS INGREDIENTES_ALERGENOS(IdIngrediente INT,IdAlergeno INT,PRIMARY KEY(IdAlergeno,IdIngrediente),FOREIGN KEY(IdAlergeno) REFERENCES ALERGENOS(IdAlergeno),FOREIGN KEY(IdIngrediente) REFERENCES INGREDIENTES(IdIngrediente));
 
-CREATE TABLE PEDIDO(
-    IdPedido INT,
-    Valoracion INT,
-    TPago VARCHAR(10) CHECK (TPago IN ('Tarjeta','Efectivo','Puntos')),
-    Estado VARCHAR(10) CHECK (Estado IN ('Activo','Inactivo')),
-    PRIMARY KEY(IdPedido)
-);
+CREATE TRIGGER ActualizarStock 
+BEFORE INSERT ON PEDIDO_RECETAS 
+FOR EACH ROW 
+BEGIN 
+    DECLARE cantidad INT; 
+    SELECT COUNT(*) INTO cantidad 
+    FROM PEDIDO_RECETAS 
+    WHERE IdReceta = NEW.IdReceta AND IdPedido = NEW.IdPedido; 
+    UPDATE Ingredientes i 
+    JOIN RECETAS_INGREDIENTES ir ON i.IdIngrediente = ir.IdIngrediente 
+    SET i.NumStock = CASE WHEN (i.NumStock - cantidad) < 0 THEN 0 ELSE (i.NumStock - cantidad) END 
+    WHERE ir.IdReceta = NEW.IdReceta; 
+END;
 
-CREATE TABLE RESERVAS(
-    IdReserva INT,
-    IdMesa INT,
-    PRIMARY KEY(IdReserva)
-);
-
-CREATE TABLE CLIENTES(
-    IdCliente INT,
-    Valoracion INT,
-    Nombre VARCHAR(40),
-    UserName VARCHAR(10),
-    Contrasenia VARCHAR(40),
-    Domicilio VARCHAR(40),
-    Puntos INT,
-    FechaNacimiento DATETIME,
-    DatosDePago VARCHAR(30),
-    PRIMARY KEY(IdCliente)
-);
-
-CREATE TABLE ALERGENOS(
-    IdAlergeno INT,
-    Nombre VARCHAR(40),
-    Descripcion VARCHAR(40),
-    PRIMARY KEY(IdAlergeno)
-);
-
-CREATE TABLE INGREDIENTES(
-    IdIngrediente INT,
-    Nombre VARCHAR(40),
-    NumStock INT CHECK (NumStock >= 0),
-    PRIMARY KEY(IdIngrediente)
-);
-
-CREATE TABLE RECETAS(
-    IdReceta INT,
-    Precio INT CHECK (Precio >= 1),
-    PRIMARY KEY(IdReceta)
-);
-
-CREATE TABLE RESERVAS_PEDIDO(
-    IdReserva INT,
-    IdPedido INT,
-    NumPersonas INT,
-    HoraIni DATETIME,
-    PRIMARY KEY(IdPedido,IdReserva),
-    FOREIGN KEY(IdReserva) REFERENCES RESERVAS(IdReserva),
-    FOREIGN KEY(IdPedido) REFERENCES PEDIDO(IdPedido)
-);
-
-CREATE TABLE TRABAJADOR_PEDIDO(
-    IdTrabajador INT,
-    IdPedido INT,
-    PRIMARY KEY(IdPedido,IdReserva),
-    FOREIGN KEY(IdPedido) REFERENCES PEDIDO(IdPedido),
-    FOREIGN KEY(IdTrabajador) REFERENCES TRABAJADOR(IdTrabajador)
-);
-
-CREATE TABLE CLIENTE_PEDIDO(
-    IdCliente INT,
-    IdPedido INT,
-    PRIMARY KEY(IdPedido,IdCliente),
-    FOREIGN KEY(IdPedido) REFERENCES PEDIDO(IdPedido),
-    FOREIGN KEY(IdCliente) REFERENCES CLIENTES(IdCliente)
-);
-
-CREATE TABLE PEDIDO_RECETAS(
-    IdReceta INT,
-    IdPedido INT,
-    PRIMARY KEY(IdPedido,IdReceta),
-    FOREIGN KEY(IdPedido) REFERENCES PEDIDO(IdPedido),
-    FOREIGN KEY(IdReceta) REFERENCES RECETAS(IdReceta)
-);
-
-CREATE TABLE CLIENTE_ALERGENOS(
-    IdCliente INT,
-    IdAlergeno INT,
-    PRIMARY KEY(IdCliente,IdAlergeno),
-    FOREIGN KEY(IdCliente) REFERENCES CLIENTES(IdCliente),
-    FOREIGN KEY(IdAlergeno) REFERENCES ALERGENOS(IdAlergeno)
-);
-
-CREATE TABLE RECETAS_INGREDIENTES(
-    IdReceta INT,
-    IdIngrediente INT,
-    PRIMARY KEY(IdReceta,IdIngrediente),
-    FOREIGN KEY(IdReceta) REFERENCES RECETAS(IdReceta),
-    FOREIGN KEY(IdIngrediente) REFERENCES INGREDIENTES(IdIngrediente)
-);
-
-CREATE TABLE INGREDIENTES_ALERGENOS(
-    IdIngrediente INT,
-    IdAlergeno INT,
-    PRIMARY KEY(IdAlergeno,IdIngrediente),
-    FOREIGN KEY(IdAlergeno) REFERENCES ALERGENOS(IdAlergeno),
-    FOREIGN KEY(IdIngrediente) REFERENCES INGREDIENTES(IdIngrediente)
-);
-
-DELIMITER //
-CREATE TRIGGER ReponerStock
-AFTER UPDATE ON INGREDIENTES
+CREATE TRIGGER IF NOT EXISTS RestarPuntos
+BEFORE INSERT ON CLIENTES_PEDIDO
 FOR EACH ROW
 BEGIN
-    IF NEW.NumStock < 10 THEN
-        UPDATE INGREDIENTES
-        SET NumStock = NEW.NumStock + 50
-        WHERE id = NEW.id
-    END;
-//
-DELIMITER ;
+    DECLARE PuntosSinActualizar INT;  
+    DECLARE TotalPuntosNecesarios INT;
+    DECLARE TipoPago VARCHAR(10);
 
-DELIMITER //
-CREATE TRIGGER ActualizarStock
+    SELECT Puntos, TPago
+    INTO PuntosSinActualizar, TipoPago
+    FROM CLIENTES
+    JOIN PEDIDO ON CLIENTES.IdCliente = NEW.IdCliente AND PEDIDO.IdPedido = NEW.IdPedido;
+
+    SELECT SUM(RECETAS.Precio)
+    INTO TotalPuntosNecesarios
+    FROM RECETAS
+    JOIN PEDIDO_RECETAS ON RECETAS.IdReceta = PEDIDO_RECETAS.IdReceta
+    WHERE PEDIDO_RECETAS.IdPedido = NEW.IdPedido;
+
+    IF TipoPago = 'Puntos' AND PuntosSinActualizar >= TotalPuntosNecesarios THEN
+        UPDATE CLIENTES
+        SET Puntos = PuntosSinActualizar - TotalPuntosNecesarios
+        WHERE IdCliente = NEW.IdCliente;
+    ELSE
+        IF TipoPago = 'Puntos' THEN
+            SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'NO TIENES LOS PUNTOS SUFICIENTES O EL TIPO DE PAGO NO ES PUNTOS';
+        END IF;
+    END IF;
+END;
+
+CREATE TRIGGER IF NOT EXISTS RellenarStock
 AFTER INSERT ON PEDIDO_RECETAS
 FOR EACH ROW
 BEGIN
-   
-    DECLARE cantidad INT;
-    SELECT COUNT(*) INTO cantidad
-    FROM PEDIDO_RECETAS
-    WHERE IdReceta = NEW.IdReceta AND IdPedido = NEW.IdPedido;
+    DECLARE NumeroActual INT;
+    SELECT NumStock
+    INTO NumeroActual
+    FROM INGREDIENTES
+    JOIN RECETAS_INGREDIENTES ON INGREDIENTES.IdIngrediente = RECETAS_INGREDIENTES.IdIngrediente
+    WHERE RECETAS_INGREDIENTES.IdReceta = NEW.IdReceta;
 
-    
-    UPDATE Ingredientes i
-    JOIN RECETAS_INGREDIENTES ir ON i.IdIngrediente = ir.IdIngrediente
-    SET i.NumStock = CASE WHEN (i.NumStock - cantidad) < 0 THEN 0 ELSE (i.NumStock - cantidad) END
-    WHERE ir.IdReceta = NEW.IdReceta;
+    IF NumeroActual-1 < 10 THEN 
+        UPDATE INGREDIENTES
+        JOIN RECETAS_INGREDIENTES ON INGREDIENTES.IdIngrediente = RECETAS_INGREDIENTES.IdIngrediente
+        SET NumStock = NumStock + 50
+        WHERE RECETAS_INGREDIENTES.IdReceta = NEW.IdReceta;
+    END IF;
 END;
-//
-DELIMITER ;
 
-DELIMITER //
-CREATE TRIGGER RestarPuntos
-BEFORE INSERT ON CLIENTE_PEDIDO
+CREATE TRIGGER IF NOT EXISTS ContieneAlergeno
+BEFORE INSERT ON CLIENTES_PEDIDO
 FOR EACH ROW
 BEGIN
+    DECLARE Peligro BOOLEAN;
+    SELECT 1 INTO Peligro
+    FROM CLIENTES_ALERGENOS
+    JOIN ALERGENOS ON CLIENTES_ALERGENOS.IdAlergeno = ALERGENOS.IdAlergeno
+    JOIN PEDIDO_RECETAS ON NEW.IdPedido = PEDIDO_RECETAS.IdPedido
+    JOIN RECETAS_INGREDIENTES ON PEDIDO_RECETAS.IdReceta = RECETAS_INGREDIENTES.IdReceta
+    JOIN INGREDIENTES_ALERGENOS ON RECETAS_INGREDIENTES.IdIngrediente = INGREDIENTES_ALERGENOS.IdIngrediente
+    WHERE CLIENTES_ALERGENOS.IdCliente = NEW.IdCliente
+    AND INGREDIENTES_ALERGENOS.IdAlergeno = ALERGENOS.IdAlergeno;
 
-    DECLARE PuntosSinActualizar INT;  
-    DECLARE Tipo VARCHAR(10);
-    DECLARE NumRestar INT;
-
-    SELECT Puntos INTO PuntosSinActualizar
-    FROM CLIENTES
-    WHERE IdCliente = NEW.IdCliente;
-
-    SELECT TPago INTO Tipo
-    FROM PEDIDO
-    WHERE IdPedido = NEW.IdPedido;
-
-    SELECT Precio INTO NumRestar
-    FROM PEDIDO
-    WHERE IdPedido = NEW.IdPedido;
-
-    IF Tipo = 'Puntos' THEN
-        IF (PuntosSinActualizar-NumRestar) < 0 THEN
-            SIGNAL SQLSTATE '4500'
-            SET MESSAGE_TEXT = 'NO TIENES LOS PUNTOS SUFICIENTES'
-        -- HACER ROLLBACK EN EL CÓDIGO A ANTES DE ENVIAR EL PEDIDO
-        ELSE
-            UPDATE CLIENTE
-            SET Puntos = PuntosSinActualizar - NumRestar
-            WHERE IdCliente = new.IdCliente;
-        END IF;
-    ELSE
+    IF Peligro THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'El cliente tiene alérgenos en las recetas del pedido';
     END IF;
-
 END;
-//
-DELIMITER ;
